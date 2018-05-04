9c80c959fe200dabab26f12c18632b4eead31f28
salmahanafy <salmahanafyahmed@gmail.com>
2018-05-04 03:06:41 +0200

    Add files via upload

--- Assembler.java
+++ Assembler.java
@@ -12,47 +12,70 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class Assembler {
-
-public static void setup(HashMap<String,String>m,HashMap<String, String>mr)
-{
-    String k[]={"INC","DEC","SUB","ADD","AND","OR","NOT","NEG","RLC","RRC","SHL","SHR","RTI","RET","POP","NU0","PUSH","NU1"
-            ,"CALL","JMP","LDD","STR","OUT","IN","JC","JN","JZ","SETC","CLRC","MOV","LDM","NOP"};
     
+    public static HashMap<String, String> Map = new HashMap<>();
+    public static HashMap<String, String> MapR = new HashMap<>();
+    public static BufferedWriter datawriter = null;
+    public static Scanner datascan = null;
+    public static BufferedWriter writer = null;
+    public static Scanner scan = null;
+
+public static void setup()
+{
+        try {
+            String k[]={"INC","DEC","SUB","ADD","AND","OR","NOT","NEG","RLC","RRC","SHL","SHR","RTI","RET","POP","NU0","PUSH","NU1"
+                    ,"CALL","JMP","LDD","OUT","IN","STD","JC","JN","JZ","SETC","CLRC","MOV","LDM","NOP"};
+            
             for(int i=0;i<32;i++)
             {
-                m.put(k[i],String.format("%05d",Integer.parseInt(Integer.toBinaryString(i))));
+                Map.put(k[i],String.format("%05d",Integer.parseInt(Integer.toBinaryString(i))));
             }
-           
+            
             String kR[]={"R0","R1","R2","R3","R4","R5","R6","R7"};
             String vR[]={"000","001","010","011","100","101","110","111"};
             for(int i=0;i<8;i++)
             {
-                mr.put(kR[i],vR[i]);
+                MapR.put(kR[i],vR[i]);
             }
+            
+            datascan = new Scanner (new File ("C:\\Users\\a\\Desktop\\data.txt"));
+            File dataMem = new File("dataMem.mem");
+            datawriter = new BufferedWriter(new FileWriter(dataMem));
+            
+            scan = new Scanner (new File ("C:\\Users\\a\\Desktop\\code.txt"));
+            File instMem = new File("instMem.mem");            
+            writer = new BufferedWriter(new FileWriter(instMem));
+            
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(Assembler.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(Assembler.class.getName()).log(Level.SEVERE, null, ex);
+        }
 }
 
-public static void main(String[] args) throws IOException {
-       
+public static void writeInstructions(BufferedWriter writer,Scanner scan,int next,int ic)
+{
+    String instruction,instOpCode;
+    String tokens[],parameters[];
+    while(scan.hasNextLine())
+    {
         try {
-            String instruction,instOpCode;
-            String tokens[],parameters[];
-            HashMap<String, String> Map = new HashMap<>();
-            HashMap<String, String> MapR = new HashMap<>();
-            setup(Map,MapR);
-            
-            File binary = new File("binary.txt");
-            BufferedWriter writer = null;
-            writer = new BufferedWriter(new FileWriter(binary));
-            
-            Scanner scan = new Scanner (new File ("C:\\Users\\a\\Desktop\\code.txt"));
-            
-            while(scan.hasNextLine())
+            instruction=scan.nextLine();
+            if(instruction.equals("."+next))
+            { 
+                while(ic<next)
+                {
+                    writer.write("0000000000000000");
+                    writer.newLine();
+                    ic++;
+                }
+            }
+            else
             {
-                instruction=scan.nextLine();
                 tokens=instruction.split(" ");
                 instOpCode=Map.get(tokens[0])+"00000";
                 //1 parameter --> duplicate
-                if(tokens[0].equals("INC")||tokens[0].equals("DEC")||tokens[0].equals("NOT")||               
+                if(tokens[0].equals("INC")||tokens[0].equals("DEC")||tokens[0].equals("NOT")||
                         tokens[0].equals("NEG")||tokens[0].equals("RLC")||tokens[0].equals("RRC")||tokens[0].equals("OUT")||
                         tokens[0].equals("IN")||tokens[0].equals("JMP")||tokens[0].equals("JC")||tokens[0].equals("JN")||
                         tokens[0].equals("JZ"))
@@ -65,7 +88,7 @@ public static void main(String[] args) throws IOException {
                 {
                     parameters=tokens[1].split(",");
                     instOpCode+=MapR.get(parameters[0])+MapR.get(parameters[1]);
-                    
+
                 }
                 //3 parameters including immediate value
                 else if(tokens[0].equals("SHL")||tokens[0].equals("SHR"))
@@ -74,26 +97,28 @@ public static void main(String[] args) throws IOException {
                     instOpCode+=MapR.get(parameters[0])+MapR.get(parameters[2]);
                     writer.write(instOpCode);
                     writer.newLine();
+                    ic++;
                     instOpCode=String.format("%016d",Integer.parseInt(Integer.toBinaryString(Integer.decode(parameters[1]))));
-                } 
+                }
                 //stack operations
                 else if (tokens[0].equals("POP")||tokens[0].equals("PUSH")||tokens[0].equals("CALL"))
                 {
-                    instOpCode+=MapR.get("R6")+MapR.get(tokens[1]);          
+                    instOpCode+=MapR.get("R6")+MapR.get(tokens[1]);
                 }
                 //RTI,RET operations
                 else if (tokens[0].equals("RTI")||tokens[0].equals("RET"))
                 {
-                    instOpCode+=MapR.get("R6")+"000";          
+                    instOpCode+=MapR.get("R6")+"000";
                 }
                 //LDD,STR,LDM operations
-                else if(tokens[0].equals("LDD")||tokens[0].equals("STR")||tokens[0].equals("LDM"))
+                else if(tokens[0].equals("LDD")||tokens[0].equals("STD")||tokens[0].equals("LDM"))
                 {
                     parameters=tokens[1].split(",");
                     instOpCode+=MapR.get(parameters[0])+MapR.get(parameters[0]);
                     writer.write(instOpCode);
                     writer.newLine();
-                    instOpCode=String.format("%016d",Integer.parseInt(Integer.toBinaryString(Integer.decode(parameters[1]))));         
+                    ic++;
+                    instOpCode=String.format("%016d",Integer.parseInt(Integer.toBinaryString(Integer.decode(parameters[1]))));
                 }
                 else//NOP,CLCR,SETC
                 {
@@ -101,12 +126,73 @@ public static void main(String[] args) throws IOException {
                 }
                 writer.write(instOpCode);
                 writer.newLine();
+                ic++;
             }
-            writer.close();
-            scan.close();
-        } catch (FileNotFoundException ex) {
+        } catch (IOException ex) {
             Logger.getLogger(Assembler.class.getName()).log(Level.SEVERE, null, ex);
         }
+            }
+ 
+    
+}
+
+public static void writeData()
+{
+        try { 
+            int dataLocation = Integer.decode(datascan.nextLine().substring(1));
+            for(int i=0; i < dataLocation ;i++)
+            {
+                datawriter.write("0000000000000000");
+                datawriter.newLine();
+            }
+            while(datascan.hasNextLine())
+            {
+                datawriter.write(String.format("%016d",Integer.parseInt(Integer.toBinaryString(Integer.decode(datascan.nextLine())))));
+                datawriter.newLine();
+            }
+            datawriter.close();
+            datascan.close();
+        } catch (IOException ex) {
+            Logger.getLogger(Assembler.class.getName()).log(Level.SEVERE, null, ex);
+        }
+}
+public static void main(String[] args) throws IOException {
+
+            
+            setup();
+            writeData();
+            
+            int startAddress=Integer.decode(scan.nextLine());           
+            writer.write(String.format("%016d",Integer.parseInt(Integer.toBinaryString(startAddress))));
+            writer.newLine();
+            
+            int ISR=Integer.decode(scan.nextLine());
+            writer.write(String.format("%016d",Integer.parseInt(Integer.toBinaryString(ISR))));
+            writer.newLine();
+            
+            if (ISR>startAddress)
+            {
+                for(int i=0;i<startAddress-2;i++)
+                {
+                    writer.write("0000000000000000");
+                    writer.newLine();
+                }
+                writeInstructions(writer,scan,ISR,startAddress);
+   
+            }
+            else
+            {
+               for(int i=0;i<ISR-2;i++)
+                {
+                    writer.write("0000000000000000");
+                    writer.newLine();
+                }
+                writeInstructions(writer,scan,startAddress,ISR);
+            }
+            
+            writer.close();
+            scan.close();
+        
     }
 }
          
